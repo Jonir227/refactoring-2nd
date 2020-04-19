7eb6db1f97fdf029cce69f2cb7a5025cb6642707
Kim Bum June <bjbj6363@gmail.com>
2020-04-19 18:43:54 +0900

    함수 추출하기

--- 01/main.js
+++ 01/main.js
@@ -1,6 +1,27 @@
 const invoice = require('./invoices.json');
 const plays = require('./plays.json');
 
+function amountFor(perf, play) {
+  let thisAmount = 0;
+  switch (play.type) {
+    case 'tragedy': // 비극
+      thisAmount = 40000;
+      if (perf.audience < 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+    case 'comedy': // 희극
+      thisAmount = 30000;
+      if (perf.audience < 20) {
+        thisAmount += 10000 + 500 * (perf.audience - 20);
+      }
+      break;
+    default:
+      throw new Error(`알 수 없는 장르 : ${play.type}`);
+  }
+  return thisAmount;
+}
+
 function statement(invoice, plays) {
   let totoalAmount = 0;
   let volumeCredits = 0;
@@ -13,24 +34,8 @@ function statement(invoice, plays) {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
+    let thisAmount = amountFor(perf, play);
 
-    switch (play.type) {
-      case 'tragedy': // 비극
-        thisAmount = 40000;
-        if (perf.audience < 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case 'comedy': // 희극
-        thisAmount = 30000;
-        if (perf.audience < 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        break;
-      default:
-        throw new Error(`알 수 없는 장르 : ${play.type}`);
-    }
     // 포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
 
@@ -48,4 +53,14 @@ function statement(invoice, plays) {
   return result;
 }
 
+console.log(
+  statement(invoice, plays).includes(
+    `청구 내역 (고객명 : BingCo)
+  Hamlet: $400.00, (55 석)
+  As You Like It: $300.00, (35 석)
+  Othello: $400.00, (40 석)
+총액: $0.00
+적립 포인트: 47점`
+  )
+);
 console.log(statement(invoice, plays));
