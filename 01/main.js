313dd8a1b3b59f2c22b226b0ecbc02aead02e33d
Kim Bum June <bjbj6363@gmail.com>
2020-04-19 19:03:41 +0900

    volumeCreditsFor 함수 변수명 변경

--- 01/main.js
+++ 01/main.js
@@ -27,12 +27,12 @@ function amountFor(aPerformance) {
 }
 
 function volumeCreditsFor(perf) {
-  let volumeCredits = 0;
-  volumeCredits += Math.max(perf.audience - 30, 0);
+  let result = 0;
+  result += Math.max(perf.audience - 30, 0);
   if ('comedy' === playFor(perf).type) {
-    volumeCredits += Math.floor(perf.audience / 5);
+    result += Math.floor(perf.audience / 5);
   }
-  return volumeCredits;
+  return result;
 }
 
 function statement(invoice) {
